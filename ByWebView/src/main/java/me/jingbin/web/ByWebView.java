fb1fad2626deab8ba35cbed7aad95fb7e4e7979a
youlookwhat <770413277@qq.com>
2021-09-05 22:04:02 +0800

    code optimization

--- ByWebView/src/main/java/me/jingbin/web/ByWebView.java
+++ ByWebView/src/main/java/me/jingbin/web/ByWebView.java
@@ -15,7 +15,6 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 
 /**
  * 网页可以处理:
