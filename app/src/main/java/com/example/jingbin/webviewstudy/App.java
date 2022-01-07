f21f6c552c6f73aae805d8e62d15cc548f6c06f6
jngbin <770413277@qq.com>
2022-01-07 16:09:11 +0800

    handle Android P针对 WebView在不同进程下无法访问非自己进程中的webview目录

--- app/src/main/java/com/example/jingbin/webviewstudy/App.java
+++ app/src/main/java/com/example/jingbin/webviewstudy/App.java
@@ -1,7 +1,12 @@
 package com.example.jingbin.webviewstudy;
 
 import android.app.Application;
+import android.content.Context;
+import android.os.Build;
 import android.util.Log;
+import android.webkit.WebView;
+
+import androidx.multidex.MultiDex;
 
 import com.squareup.leakcanary.LeakCanary;
 import com.tencent.smtt.sdk.QbSdk;
@@ -48,4 +53,28 @@ public class App extends Application {
         QbSdk.initX5Environment(getApplicationContext(),  cb);
     }
 
+    /**
+     * 方法数超64k 解决 https://developer.android.com/studio/build/multidex?hl=zh-cn
+     * 继承 MultiDexApplication 或 实现此方法。
+     */
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        initWebView();
+        MultiDex.install(this);
+    }
+
+    /**
+     * Android P针对 WebView在不同进程下无法访问非自己进程中的webview目录
+     * fix Using WebView from more than one process at once with the same data directory is not supported
+     */
+    private void initWebView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            String processName = getProcessName();
+            String packageName = this.getPackageName();
+            if (!packageName.equals(processName)) {
+                WebView.setDataDirectorySuffix(processName);
+            }
+        }
+    }
 }
