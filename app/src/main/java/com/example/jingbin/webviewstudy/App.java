60197559b91f2e435d6a578df1166d84297b358f
jngbin <770413277@qq.com>
2022-02-21 11:03:53 +0800

    update   非wifi条件下允许下载X5内核

--- app/src/main/java/com/example/jingbin/webviewstudy/App.java
+++ app/src/main/java/com/example/jingbin/webviewstudy/App.java
@@ -34,13 +34,17 @@ public class App extends Application {
     }
 
     private void initX5() {
+        // 非wifi条件下允许下载X5内核
+        QbSdk.setDownloadWithoutWifi(true);
         //搜集本地tbs内核信息并上报服务器，服务器返回结果决定使用哪个内核。
         QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
 
             @Override
             public void onViewInitFinished(boolean arg0) {
                 //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
-                Log.d("app", " onViewInitFinished is " + arg0);
+                if (!arg0) {
+                    Log.e("ByWebView", "x5内核加载失败，自动切换到系统内核");
+                }
             }
 
             @Override
